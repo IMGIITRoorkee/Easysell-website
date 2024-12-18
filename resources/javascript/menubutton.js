d79624a563e6714cce9b5df513e1eb3e3031f218
anshkarwasra <anshkarwasra15@gmail.com>
2024-12-18 20:50:33 +0530

    added try us out button adjecent to the hamburger

--- resources/javascript/menubutton.js
+++ resources/javascript/menubutton.js
@@ -2,10 +2,13 @@ var i = 0;
 
 function show_hide() {
     if (i == 1) {
-        document.querySelector('.main-nav').style.display = "flex";
+        document.querySelector('.main-nav').style.left = '120%';
+        document.querySelector('#li3').style.left = '-120%';
         return i = 0;
     } else {
-        document.querySelector('.main-nav').style.display = "none";
+        console.log('ya')
+        document.querySelector('.main-nav').style.left = '0%';
+        document.querySelector('#li3').style.left = '-13.9%';
         return i = 1;
     }
 }

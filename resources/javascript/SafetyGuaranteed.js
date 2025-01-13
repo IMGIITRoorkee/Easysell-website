fa4e9936bb7c19cb2aa884710acfeab4fd56b491
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2025-01-13 16:57:08 +0530

    Update SafetyGuaranteed.js

--- resources/javascript/SafetyGuaranteed.js
+++ resources/javascript/SafetyGuaranteed.js
@@ -10,13 +10,6 @@ for (let i = 0; i < safetyItems.length; i++) {
             safetyItems[i].classList.add("active");
         }
     });
-
-    safetyDivItems.addEventListener("mouseover", function () {
-        if (!safetyItems[i].classList.contains("active")) {
-            safetyItems[i].classList.add("active");
-        }
-    });
-
     safetyItems[i].addEventListener("mouseout", function (e) {
         // Collapse only if pointer is outside the current .safety div
         if (!safetyItems[i].contains(e.relatedTarget)) {
